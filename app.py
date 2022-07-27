36408e583a20efef49f8c8a13dfdd5097aa5b13f
Sam Snarr <38705645+sss-ng@users.noreply.github.com>
2022-07-27 19:55:31 -0400

    reactivated some important endpoints(urls)

--- app.py
+++ app.py
@@ -9,25 +9,29 @@ from random import randint
 app = Flask(__name__)
 
 
-# @app.get("/hello")
-# def hello_world():
-#     return "Hello, World!"
+@app.get("/hello")
+def hello_world():
+    return "Hello, World!"
 
-# @app.get("/clyde")
-# def dog():
-#     return "Snaggle tooth"
 
-# @app.get("/random")
-# def random():
-#     return str(randint(0,10))
+@app.get("/clyde")
+def dog():
+    return "Snaggle tooth"
 
-# @app.get("/test")
-# def test():
-#     return str([3,6,2,9])
+
+@app.get("/random")
+def random():
+    return str(randint(0, 10))
+
+
+@app.get("/test")
+def test():
+    return str([3, 6, 2, 9])
+
 
 @app.get("/is_Even")
 def is_Even():
-    num = request.args.get('number')
+    num = request.args.get("number")
     if num is None:
         print("The user forgot to send a number.")
         return "Error! Please send a number by putting ?number=(input) after url."
@@ -38,8 +42,6 @@ def is_Even():
         return "Is even!"
     else:
         return "Is odd!"
-    
+
 
 app.run(debug=True, port=5000)
-
-
