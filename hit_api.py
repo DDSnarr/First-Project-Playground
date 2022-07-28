195f3f9bfb6a611d577bb90b9673f7f1b691bd9a
DDSnarr <ddsnarr@gmail.com>
2022-07-27 20:36:04 -0400

    Changed from Localhost to 127.0.0.1 It greatly increased the speed, unsure as to why it did this

--- hit_api.py
+++ hit_api.py
@@ -3,8 +3,8 @@ import requests
 d = {}
 arr = []
 
-for i in range(100):
-    response = requests.get("http://localhost:5000/is_Even?number=" + str(i))
+for i in range(10000):
+    response = requests.get("http://127.0.0.1:5000/is_Even?number=" + str(i))
     print(i, "response is ", response.content)
 
     d[i] = response.content
